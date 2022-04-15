391f42fcdb3ad98d93358eac4844b386b99ed614
saiklayan <askalyan06@gmail.com>
2022-04-15 10:55:07 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.db.models.functions import Length
+from app.models import *
 # Create your views here.
+def display_topics(request):
+    topics=Topic.objects.all()
+    d={'ts':topics}
+    return render(request,'display_topics.html',d)
+
+def display_webpage(request):
+    webpages=Webpage.objects.all()
+    webpages=Webpage.objects.filter(topic_name='pubg')
+    webpages=Webpage.objects.all().order_by('name')
+    webpages=Webpage.objects.all().order_by('-name')
+    webpages=Webpage.objects.all().order_by(Length('name'))
+    webpages=Webpage.objects.all().order_by(Length('name').desc())
+    d={'ws':webpages}
+    return render(request,'display_webpage.html',d)
+
+
+def display_Access(request):
+    Access=AccesRecords.objects.all()
+    Access=AccesRecords.objects.all().order_by('date')
+    d={'as':Access}
+    return render(request,'display_Access.html',d)
--- project17/urls.py
+++ project17/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('display_topics/',display_topics,name='display_topics'),
+    path('display_webpage/',display_webpage,name='display_webpage'),
+    path('display_Access/',display_Access,name='display_Access'),
 ]
